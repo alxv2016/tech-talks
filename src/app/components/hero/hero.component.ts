0fe2e71c6fa701ba8bbf995ca5a7e81091977584
Alex Vong <avong2011@gmail.com>
2021-03-19 07:56:57 -0400

    feat: added hero animations

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -25,25 +25,45 @@ export class HeroComponent implements AfterViewInit {
   // @HostBinding('style.--a-end') @Input() aEnd: string = '0%';
   // @HostBinding('style.--b-start') @Input() bStart: string = '0%';
   // @HostBinding('style.--b-end') @Input() bEnd: string = '0%';
-  // @ViewChild('heroImage') heroImage!: ElementRef;
+  @ViewChild('heroTitle') heroTitle!: ElementRef;
   @ViewChildren('grad', {read: ElementRef}) grad!: QueryList<ElementRef>;
 
   constructor(private element: ElementRef, private render: Renderer2, private util: UtilityService) {}
 
   ngAfterViewInit(): void {
+    const heroTitle = this.heroTitle.nativeElement;
     const grads = this.grad.map((grad) => grad.nativeElement);
 
-    gsap.fromTo(
-      grads,
-      {
-        xPercent: -90,
-      },
-      {
-        xPercent: 90,
-        stagger: 0.25,
+    const glitch = gsap.timeline({
+      defaults: {
         yoyo: true,
+        yoyoEase: true,
         repeat: -1,
-      }
-    );
+        duration: 4.75,
+        ease: 'back',
+      },
+    });
+
+    glitch
+      .fromTo(
+        grads,
+        {
+          xPercent: -80,
+        },
+        {
+          xPercent: 80,
+          stagger: 0.125,
+        }
+      )
+      .fromTo(
+        heroTitle,
+        {
+          textShadow: '-1.25px 0px 0px #12FFFF, 1.25px 0px 0px #FF00A2',
+        },
+        {
+          textShadow: '1.25px 0px 0px #12FFFF, -1.25px 0px 0px #FF00A2',
+        },
+        3.25
+      );
   }
 }
