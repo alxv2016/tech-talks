7fd93f2c430ce4a98d95416dbb087e60f147c9eb
Alex Vong <avong2011@gmail.com>
2021-03-21 19:13:54 -0400

    feat: added hosts section

---
+++ src/app/sections/hosts/hosts.component.ts
@@ -0,0 +1,63 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import {gsap} from 'gsap';
+import {UtilityService} from 'src/app/services/utility.service';
+
+@Component({
+  selector: 'c-hosts',
+  templateUrl: './hosts.component.html',
+  styleUrls: ['./hosts.component.scss'],
+})
+export class HostsComponent implements OnInit, AfterViewInit {
+  @HostBinding('class') class = 'c-hosts l-content--reveal';
+  @HostBinding('style.--a-start') @Input() aStart: string = '0%';
+  @HostBinding('style.--a-end') @Input() aEnd: string = '0%';
+  @HostBinding('style.--b-start') @Input() bStart: string = '0%';
+  @HostBinding('style.--b-end') @Input() bEnd: string = '0%';
+  @ViewChild('brandTrigger') brandTrigger!: ElementRef;
+  @ViewChildren('introTitle', {read: ElementRef}) introTitle!: QueryList<ElementRef>;
+  @ViewChildren('grad', {read: ElementRef}) grad!: QueryList<ElementRef>;
+  constructor(private element: ElementRef, private render: Renderer2, private util: UtilityService) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const grads = this.grad.map((grad) => grad.nativeElement);
+    const glitch = gsap.timeline({
+      defaults: {
+        yoyo: true,
+        yoyoEase: true,
+        repeat: -1,
+        ease: 'back',
+        duration: 0.65,
+      },
+    });
+
+    glitch.fromTo(
+      grads,
+      {
+        yPercent: -1,
+        opacity: 0.45,
+      },
+      {
+        yPercent: 1,
+        opacity: 1,
+        stagger: {
+          each: 0.125,
+          from: 'start',
+        },
+      }
+    );
+  }
+}
--- src/app/sections/sections.module.ts
+++ src/app/sections/sections.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {ValuePropComponent} from './value-prop/value-prop.component';
 import {IntroComponent} from './intro/intro.component';
 import {FigmaComponent} from './figma/figma.component';
+import {HostsComponent} from './hosts/hosts.component';
 
 @NgModule({
-  declarations: [ValuePropComponent, IntroComponent, FigmaComponent],
-  exports: [ValuePropComponent, IntroComponent, FigmaComponent],
+  declarations: [ValuePropComponent, IntroComponent, FigmaComponent, HostsComponent],
+  exports: [ValuePropComponent, IntroComponent, FigmaComponent, HostsComponent],
   imports: [CommonModule],
 })
 export class SectionsModule {}
