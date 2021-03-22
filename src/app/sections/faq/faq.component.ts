1bfd2c36c1f5c46b1739613b8f6366f3ff7ed333
Alex Vong <avong2011@gmail.com>
2021-03-22 11:45:26 -0400

    feat: styled faq section

--- src/app/sections/faq/faq.component.ts
+++ src/app/sections/faq/faq.component.ts
@@ -34,20 +34,24 @@ export class FaqComponent implements OnInit {
   ngOnInit(): void {
     this.demoData = [
       {
-        title: 'title 1',
-        descr: 'You could write the de-sugared version directly like so:',
+        title: 'How long is this Tech Talks session?',
+        descr:
+          'This session will be 1 hour 15 minutes long as we will be diving into Figma to design and prototype an app to learn the core features of Figma. You will have 15 minutes at the end for a Q and A session.',
       },
       {
-        title: 'title 2',
-        descr: 'You could write the de-sugared version directly like so:',
+        title: 'Do I need to install Figma?',
+        descr:
+          'Figma is cross platform and can also run in the web browser.  While you do not need to install Figma, we highly suggest that you do because it will run alot better than the web app.',
       },
       {
-        title: 'title 3',
-        descr: 'You could write the de-sugared version directly like so:',
+        title: 'What if I already know how to use Figma?',
+        descr:
+          'This session is geared towards designers who are new to Figma, however Tech Talks sessions are aim to combine development practices into design so you may still learn some new things.',
       },
       {
-        title: 'title 4',
-        descr: 'You could write the de-sugared version directly like so:',
+        title: "What if I can't join this session?",
+        descr:
+          "We know the seats are limited to 10 people, however we will be recording this session so if you couldn't attend you can still watch the recording and stay tuned for the next Tech Talks.",
       },
     ];
   }
