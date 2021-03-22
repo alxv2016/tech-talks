1b8eea1f89c546e9b103ce35262b668b3656fc3b
Alex Vong <avong2011@gmail.com>
2021-03-22 11:35:52 -0400

    feat: added accordion component

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,13 +1,51 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'c-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss'],
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent implements AfterViewInit {
+  expanded = false;
   @HostBinding('class') class = 'c-accordion';
-  constructor() {}
+  @Input() faqData: any;
+  @ViewChildren('accordionHeader', {read: ElementRef}) accordionHeader!: QueryList<ElementRef>;
+  @ViewChildren('accordionPanel', {read: ElementRef}) accordionPanel!: QueryList<ElementRef>;
+  constructor(private element: ElementRef, private render: Renderer2) {}
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    const headers = this.accordionHeader.map((header) => header.nativeElement);
+    const panels = this.accordionPanel.map((panel) => panel.nativeElement);
+    panels.forEach((panel) => {
+      this.render.setAttribute(panel, 'style', 'height: 0; visibility: hidden;');
+    });
+    headers.forEach((header) => {
+      this.render.setAttribute(header, 'aria-expanded', `${this.expanded}`);
+    });
+  }
+
+  togglePanel(ev: any, i: number): void {
+    console.log(ev.target);
+    const panels = this.accordionPanel.map((panel) => panel.nativeElement);
+    const panelHeight = panels[i].scrollHeight;
+    if (ev.target.getAttribute('aria-expanded') === 'true') {
+      this.render.setAttribute(ev.target, 'aria-expanded', `${this.expanded}`);
+      this.render.removeClass(ev.target, 'is-opened');
+      this.render.setAttribute(panels[i], 'style', 'height: 0; visibility: hidden;');
+    } else {
+      this.render.setAttribute(ev.target, 'aria-expanded', `${!this.expanded}`);
+      this.render.addClass(ev.target, 'is-opened');
+      this.render.setAttribute(panels[i], 'style', `height: ${panelHeight}px; visibility: visible;`);
+    }
+  }
 }
--- src/app/sections/faq/faq.component.ts
+++ src/app/sections/faq/faq.component.ts
@@ -20,6 +20,8 @@ import {UtilityService} from 'src/app/services/utility.service';
   styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent implements OnInit {
+  demoData: any = [];
+
   @HostBinding('class') class = 'c-intro l-content--reveal';
   @HostBinding('style.--a-start') @Input() aStart: string = '0%';
   @HostBinding('style.--a-end') @Input() aEnd: string = '0%';
@@ -29,5 +31,24 @@ export class FaqComponent implements OnInit {
   @ViewChildren('introTitle', {read: ElementRef}) introTitle!: QueryList<ElementRef>;
   constructor(private element: ElementRef, private render: Renderer2, private util: UtilityService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.demoData = [
+      {
+        title: 'title 1',
+        descr: 'You could write the de-sugared version directly like so:',
+      },
+      {
+        title: 'title 2',
+        descr: 'You could write the de-sugared version directly like so:',
+      },
+      {
+        title: 'title 3',
+        descr: 'You could write the de-sugared version directly like so:',
+      },
+      {
+        title: 'title 4',
+        descr: 'You could write the de-sugared version directly like so:',
+      },
+    ];
+  }
 }
