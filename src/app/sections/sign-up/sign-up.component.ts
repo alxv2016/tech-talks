69733d221bf471c955fed9a2a12b57517fc07674
Alex Vong <avong2011@gmail.com>
2021-03-25 07:06:16 -0400

    feat: added forms validations

--- src/app/sections/sign-up/sign-up.component.ts
+++ src/app/sections/sign-up/sign-up.component.ts
@@ -11,6 +11,7 @@ import {
   ViewChild,
   ViewChildren,
 } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {gsap} from 'gsap';
 import {UtilityService} from 'src/app/services/utility.service';
 
@@ -20,6 +21,13 @@ import {UtilityService} from 'src/app/services/utility.service';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit, AfterViewInit {
+  signupForm: FormGroup;
+  reserved = false;
+  errors = {
+    firstName: false,
+    lastName: false,
+    skillLevel: false,
+  };
   @HostBinding('class') class = 'c-sign-up l-content--reveal';
   @HostBinding('style.--a-start') @Input() aStart: string = '0%';
   @HostBinding('style.--a-end') @Input() aEnd: string = '0%';
@@ -27,9 +35,46 @@ export class SignUpComponent implements OnInit, AfterViewInit {
   @HostBinding('style.--b-end') @Input() bEnd: string = '0%';
   @ViewChild('brandTrigger') brandTrigger!: ElementRef;
   @ViewChildren('introTitle', {read: ElementRef}) introTitle!: QueryList<ElementRef>;
-  constructor(private element: ElementRef, private render: Renderer2, private util: UtilityService) {}
+  constructor(
+    private element: ElementRef,
+    private render: Renderer2,
+    private util: UtilityService,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.checkLocalStorage());
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i), this.validateSpaces]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i), this.validateSpaces]],
+      skillLevel: ['', [Validators.required]],
+      comments: '',
+    });
+  }
 
   ngAfterViewInit(): void {}
+
+  private validateSpaces(control: FormControl) {
+    const valid = (control.value || '').trim().length !== 0;
+    return valid ? null : {whitespace: true};
+  }
+
+  signUp() {
+    console.clear();
+    for (const k in this.signupForm.controls) {
+      this.errors[k] = this.checkError(k);
+    }
+    if (this.signupForm.status === 'VALID') {
+      console.log('Valid store local storage and submit');
+      this.reserved = true;
+    }
+  }
+
+  checkError(field: string): boolean {
+    return this.signupForm.get(field).errors !== null;
+  }
+
+  checkLocalStorage(): boolean {
+    return localStorage.getItem('reserved') === null ? false : true;
+  }
 }
