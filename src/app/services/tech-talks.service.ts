541924ab9bdfc2a3d5dc39ee55aeff41db10d0f1
Alex Vong <avong2011@gmail.com>
2021-03-26 09:22:18 -0400

    fix: writing up sign up logic

--- src/app/services/tech-talks.service.ts
+++ src/app/services/tech-talks.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {forkJoin, Observable, of} from 'rxjs';
-import {map, switchMap} from 'rxjs/operators';
+import {catchError, map, switchMap} from 'rxjs/operators';
 import {environment} from 'src/environments/environment';
 import {GuestList, ReservedSeats, SignupInfo} from './models/tach-talks.interface';
 
@@ -10,51 +10,84 @@ import {GuestList, ReservedSeats, SignupInfo} from './models/tach-talks.interfac
 })
 export class TechTalksService {
   private ep = environment.API_URL;
-
   constructor(private http: HttpClient) {}
 
-  checkGuestList(userInfo: SignupInfo): Observable<boolean> {
-    const $guestList = this.http.get<GuestList>(`${this.ep}/guest_list`, {responseType: 'json'});
-    const $reservations = this.http.get<ReservedSeats>(`${this.ep}/sign_up`, {responseType: 'json'});
-    return $guestList.pipe(
-      switchMap((guests) => {
-        if (
-          guests.data.length <= 10 &&
-          guests.data.some((guest) => guest.first_name === userInfo.first_name) &&
-          guests.data.some((guest) => guest.last_name === userInfo.last_name)
-        ) {
-          return $reservations.pipe(
-            map((reservations) => {
-              if (
-                reservations.data.length !== 0 &&
-                reservations.data.some((guest) => guest.first_name === userInfo.first_name) &&
-                reservations.data.some((guest) => guest.last_name === userInfo.last_name)
-              ) {
-                return false;
-              }
-              return true;
-            })
-          );
-        }
-        return of(false);
-      })
-    );
+  checkReservations(limit: number): Observable<any> {
+    const signUpClosed = {
+      guests: [],
+      closed: true,
+      error: false,
+    };
+    return this.http
+      .get<ReservedSeats>(`${this.ep}/sign_up`, {responseType: 'json'})
+      .pipe(
+        map((seats) => {
+          signUpClosed.guests = seats.data;
+          if (seats.data.length !== 0 && seats.data.length <= limit) {
+            signUpClosed.closed = false;
+            return signUpClosed;
+          }
+          return signUpClosed;
+        }),
+        catchError((error) => {
+          signUpClosed.error = true;
+          return of(signUpClosed);
+        })
+      );
   }
 
-  signUp(userInfo: SignupInfo): Observable<any> {
-    return this.checkGuestList(userInfo).pipe(
-      switchMap((availableSeat) => {
-        if (availableSeat) {
+  checkGuestList(signupInfo: SignupInfo): Observable<any> {
+    const canadaStudio = {
+      guestList: [],
+      onList: false,
+      error: true,
+    };
+    return this.http
+      .get<GuestList>(`${this.ep}/guest_list`, {responseType: 'json'})
+      .pipe(
+        map((guestList) => {
+          if (
+            guestList.data.length !== 0 &&
+            guestList.data.some((guest) => guest.first_name === signupInfo.first_name) &&
+            guestList.data.some((guest) => guest.last_name === signupInfo.last_name)
+          ) {
+            canadaStudio.guestList = guestList.data;
+            canadaStudio.onList = true;
+            canadaStudio.error = false;
+            return canadaStudio;
+          }
+          canadaStudio.guestList = guestList.data;
+          canadaStudio.error = false;
+          return canadaStudio;
+        }),
+        catchError((error) => {
+          return of(canadaStudio);
+        })
+      );
+  }
+
+  signUp(signupInfo: SignupInfo): Observable<any> {
+    const $guestList = this.checkGuestList(signupInfo);
+    return $guestList.pipe(
+      switchMap((guestList) => {
+        if (!guestList.error && guestList.onList) {
           return this.http
-            .post<SignupInfo>(`${this.ep}/sign_up`, userInfo, {responseType: 'json'})
+            .post<ReservedSeats>(`${this.ep}/sign_up`, signupInfo, {responseType: 'json'})
             .pipe(
-              switchMap((_) => {
-                return this.http.get<ReservedSeats>(`${this.ep}/sign_up`, {responseType: 'json'});
+              map((data) => {
+                return data;
+              }),
+              catchError((error) => {
+                // If user already signed up
+                if (error.error.error.code === 204) {
+                  return of('Already signed up');
+                }
+                // API error
+                return of(error.error.error.code);
               })
             );
         }
-        //
-        return this.http.get<ReservedSeats>(`${this.ep}/sign_up`, {responseType: 'json'});
+        return of(guestList);
       })
     );
   }
