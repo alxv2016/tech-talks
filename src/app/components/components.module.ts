3591543d26efcc46467a376c136192074e052647
Alex Vong <avong2011@gmail.com>
2021-03-31 19:45:20 -0400

    feat: added scroll indicator

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,10 +5,25 @@ import {HeroComponent} from './hero/hero.component';
 import {AccordionComponent} from './accordion/accordion.component';
 import {FooterComponent} from './footer/footer.component';
 import {WarningComponent} from './warning/warning.component';
+import {ScrollIndicatorComponent} from './scroll-indicator/scroll-indicator.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HeroComponent, AccordionComponent, FooterComponent, WarningComponent],
-  exports: [HeaderComponent, HeroComponent, AccordionComponent, FooterComponent, WarningComponent],
+  declarations: [
+    HeaderComponent,
+    HeroComponent,
+    AccordionComponent,
+    FooterComponent,
+    WarningComponent,
+    ScrollIndicatorComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    HeroComponent,
+    AccordionComponent,
+    FooterComponent,
+    WarningComponent,
+    ScrollIndicatorComponent,
+  ],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
---
+++ src/app/components/scroll-indicator/scroll-indicator.component.ts
@@ -0,0 +1,82 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  OnInit,
+  QueryList,
+  Renderer2,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+@Component({
+  selector: 'c-scroll-indicator',
+  templateUrl: './scroll-indicator.component.html',
+  styleUrls: ['./scroll-indicator.component.scss'],
+})
+export class ScrollIndicatorComponent implements AfterViewInit {
+  @HostBinding('class') class = 'c-scroll-indicator';
+  @ViewChild('scrollIndicator') scrollIndicator!: ElementRef;
+  constructor(private element: ElementRef, private render: Renderer2) {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  private initGsap(): void {
+    const scrollIndicate = gsap.timeline({
+      defaults: {
+        ease: 'power3',
+        duration: 4.4,
+        repeat: -1,
+        stagger: {
+          each: 0.125,
+          from: 'end',
+        },
+      },
+    });
+
+    scrollIndicate.fromTo(
+      this.scrollIndicator.nativeElement,
+      {
+        yPercent: -100,
+        scaleY: 1,
+        opacity: 0,
+      },
+      {
+        yPercent: 100,
+        scaleY: 0.25,
+        opacity: 1,
+      }
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.initGsap();
+
+    const tl = gsap.to(this.element.nativeElement, {
+      ease: 'power1',
+      duration: 0.25,
+      yPercent: 100,
+      opacity: 0,
+      visibility: 'hidden',
+    });
+    tl.pause();
+
+    const indicator = this.element.nativeElement;
+    ScrollTrigger.create({
+      onUpdate: (self: any) => {
+        const scrollPos = self.scroller.pageYOffset;
+        if (indicator) {
+          const headerHeight = indicator.getBoundingClientRect().height;
+          if (scrollPos >= headerHeight) {
+            tl.play();
+          } else {
+            tl.reverse();
+          }
+        }
+      },
+    });
+  }
+}
